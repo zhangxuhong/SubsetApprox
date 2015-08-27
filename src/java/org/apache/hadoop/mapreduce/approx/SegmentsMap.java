5515445843a0af42401285b6ba145f39eed41919
Xuhong Zhang <xzhang@eecs.ucf.edu>
2015-08-27 18:06:16 -0400

    add segmentsmap.java for storing frequency statistics

---
+++ src/java/org/apache/hadoop/mapreduce/approx/SegmentsMap.java
@@ -0,0 +1,45 @@
+package org.apache.hadoop.mapreduce.approx;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.Map;
+
+public class SegmentsMap {
+
+	private static class Segment {
+  		private long offset;
+  		private long length;
+  		private String keys;
+
+  		public void addkey(String onekey){
+
+  			keys = keys + "\t" + onekey;
+  		}
+
+  		public long getOffset(){
+  			return offset;
+  		}
+  		public long getLength(){
+  			return length;
+  		}
+  	}
+
+
+
+  	public Segment[] getSampleSegmentsList(double ratio){
+
+  		return null;
+  	}
+
+  	public Segment[] getSampleSegmentsList(double error, double s2, double confidence){
+
+  		return null;
+  	}
+}
